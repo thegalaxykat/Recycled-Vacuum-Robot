25f84dee2a159d8210d440e4940790d3b1ecaeab
Kat Canavan <Katcanavan@gmail.com>
2021-05-10 15:43:07 -0400

    motor drives

--- RobotDrive.py
+++ RobotDrive.py
@@ -1,12 +1,21 @@
-from gpiozero import Motor
-from time import sleep
 #import SocketServer as Server
+from gpiozero import Motor, LED
+from time import sleep
+from signal import pause
 
-commands = Server.Drive_Command_List
-print(commands)
+# Right motor GPIO pins 26,20
+Rforward = 26
+Rbackward = 20
 
+# Left motor GPIO pins 19,16
+Lforward = 19
+Lbackward = 16
 
-#Right_Wheel = Motor(,)
-#Left_Wheel = Motor(,)
+left_wheel = Motor(Lforward, Lbackward)
+right_wheel = Motor(Rforward,Rbackward)
 
-# while True:
+while True:
+    right_wheel.forward()
+    sleep(2)
+    right_wheel.backward()
+    sleep(2)
