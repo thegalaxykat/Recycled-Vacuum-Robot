c0db66db3445cad83734c1ca69c854f4e8f3cf46
Kat Canavan <Katcanavan@gmail.com>
2021-05-13 14:18:55 -0400

    Client python script

--- Client Side/ClientScript.py
+++ Client Side/ClientScript.py
@@ -1,1 +1,15 @@
 import socket
+
+serversocket = socket.socket()
+
+serversocket.connect (('172.16.177.148', 1776)) #connect to server
+print("connected")
+
+#prompt user for direction and time 
+direction = input("Robot Direction: ")
+time = input("Drive Time in Seconds: ")
+commands = (direction + ', ' + time)
+data = commands.encode("utf-8")
+
+serversocket.send(data)
+serversocket.close
